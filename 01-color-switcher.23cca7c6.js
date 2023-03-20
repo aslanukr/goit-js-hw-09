0ad3f0ab29f82f97493ba6039788ebe6a5dbc09d
aslanukr <114025378+aslanukr@users.noreply.github.com>
2023-03-20 20:32:58 +0000

    Deploying to gh-pages from @ aslanukr/goit-js-hw-09@0125b60d93018f4a0158628f2279302b46e1c689 🚀

--- 01-color-switcher.23cca7c6.js
+++
@@ -1,2 +0,0 @@
-console.log("Знову під сам дедлайн буде домашка....(((");
-//# sourceMappingURL=01-color-switcher.23cca7c6.js.map
--- 01-color-switcher.c87ddd3f.js
+++
@@ -1,2 +0,0 @@
-console.log("Знову під сам дедлайн буде домашка....(((");
-//# sourceMappingURL=01-color-switcher.c87ddd3f.js.map
