0125b60d93018f4a0158628f2279302b46e1c689
Vitaliy Korostylov <vitaliy.koros@gmail.com>
2023-03-20 22:32:31 +0200

    completes task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,39 @@
-console.log('Знову під сам дедлайн буде домашка....(((');
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
+
+let timerId = null;
+
+refs.startBtn.addEventListener('click', () => {
+  startColorSwitcher(), 1000;
+});
+
+refs.stopBtn.addEventListener('click', () => {
+  stopColorSwitcher();
+});
+
+function startColorSwitcher() {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  if (timerId) {
+    refs.startBtn.setAttribute('disabled', '');
+    refs.stopBtn.removeAttribute('disabled', '');
+  }
+}
+
+function stopColorSwitcher() {
+  clearInterval(timerId);
+
+  if (timerId) {
+    refs.stopBtn.setAttribute('disabled', '');
+    refs.startBtn.removeAttribute('disabled', '');
+  }
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
